a96c4feed8595f823cd93758862cbe0ea8cd1d91
angelfits <stevenangelpaucartorrejon@gmail.com>
2026-01-31 14:49:09 -0500

    refactor: reorganizar como proyecto multi-módulo con Eureka Server y API Gateway

---
+++ eureka-server/src/test/java/com/enterprise/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
